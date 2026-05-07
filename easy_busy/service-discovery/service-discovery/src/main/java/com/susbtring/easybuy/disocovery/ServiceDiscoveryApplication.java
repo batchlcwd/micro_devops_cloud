6b682fd275239f050c62bb6b62de4860875c4ea3
batchlcwd <batchlcwd@gmail.com>
2026-05-07 22:51:42 +0530

    discovery-server added

---
+++ easy_busy/service-discovery/service-discovery/src/main/java/com/susbtring/easybuy/disocovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.susbtring.easybuy.disocovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//Enable the eureka server
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}
